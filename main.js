06c556c569d8336be3c6e72272362dd603bd7e38
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2025-02-04 09:42:47 -0500

    BinarySearch Basic implementation

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 let hello = "hello"
+
+//Binary Search
+
+let arr = [4, 7, 12, 24, 32, 56, 63, 94]
+
+function binarySearch(arr = [], x) {
+  let start = 0
+  let end = arr.length
+
+  while (start <= end) {
+    let mid = (start + end) / 2
+    if (arr[mid] === x) {
+      return mid
+    } else if (x > arr[mid]) {
+      start = mid + 1
+    } else {
+      end = mid + 1
+    }
+  }
+  return -1
+}
+
+console.log("Value at index: " + binarySearch(arr, 12))
