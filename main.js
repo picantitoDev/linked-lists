b035b6767f9f05615c14fc370b8eb6d053dffafc
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2025-02-04 09:54:10 -0500

    Binary search Iterative/Recursive implementation

--- main.js
+++ main.js
@@ -6,19 +6,33 @@ let arr = [4, 7, 12, 24, 32, 56, 63, 94]
 
 function binarySearch(arr = [], x) {
   let start = 0
-  let end = arr.length
+  let end = arr.length - 1
 
   while (start <= end) {
-    let mid = (start + end) / 2
+    let mid = Math.floor((start + end) / 2)
     if (arr[mid] === x) {
       return mid
     } else if (x > arr[mid]) {
       start = mid + 1
     } else {
-      end = mid + 1
+      end = mid - 1
     }
   }
   return -1
 }
 
-console.log("Value at index: " + binarySearch(arr, 12))
+function binarySearchRec(arr = [], x, start = 0, end = arr.length - 1) {
+  let mid = Math.floor((start + end) / 2)
+  if (start > end) {
+    return -1
+  }
+  if (arr[mid] === x) {
+    return mid
+  } else if (x > arr[mid]) {
+    return binarySearchRec(arr, x, mid + 1, end)
+  } else {
+    return binarySearchRec(arr, x, start, mid - 1)
+  }
+}
+
+console.log("Value at index: " + binarySearchRec(arr, 12))
